727950cfe257ad1b0fb0b17a62dfb1c8dbc7b5f4
Jake Howard <jhoward@sourcebots.co.uk>
2024-08-07 14:57:47 +0100

    Add time slide

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import Reveal from 'reveal.js';
 
+function setTime() {
+    const time = (new Date()).toLocaleTimeString();
+
+    for (const e of document.getElementsByClassName("current-time")) {
+        e.innerText = time;
+    }
+}
+
 window.addEventListener("load", function() {
     let deck = new Reveal({});
     deck.initialize({
@@ -8,4 +16,6 @@ window.addEventListener("load", function() {
         hash: true,
         controls: false,
     });
+
+    setInterval(setTime, 500);
 });
